e6aede46c12a37a946a049018885bcfa94a9944b
d-fabian <fabian.dungs@gmx.de>
2021-11-29 13:24:02 +0100

    add story for rednerliste

---
+++ stories/Rednerliste.stories.js
@@ -0,0 +1,10 @@
+import '../woche08/rednerliste.js'
+
+export default {
+    title: 'Komponenten/Rednerliste',
+}
+
+const Template = (() => `<rednerliste-module></rednerliste-module>`)
+
+export const rednerliste = Template.bind({})
+rednerliste.args = {}
