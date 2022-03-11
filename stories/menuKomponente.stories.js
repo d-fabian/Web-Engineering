747ae519da2952fea577f5d82f5a11a8e8dc3fce
d-fabian <fabian.dungs@gmx.de>
2022-03-11 12:33:30 +0100

    fix menuKomponente Story

--- stories/menuKomponente.stories.js
+++ stories/menuKomponente.stories.js
@@ -1,10 +1,19 @@
-import './menuKomponente'
+import MenuKomponente from "./../src/components/MenuKomponente";
 
 export default {
-    title: 'Komponenten/Menu Komponente'
+    title: 'Komponenten/Menu Komponente',
+    component: MenuKomponente,
 }
 
-const Template = (() => `<menu-komponente></menu-komponente>`)
+const Template = (args, { argTypes }) => ({
+    props: Object.keys(argTypes),
+    components: { MenuKomponente },
+    template: `<menu-komponente :is-vertical="$props.isVertical" :button-labels="$props.buttonLabels"></menu-komponente>`
+});
 
-export const menuKomponente = Template.bind({})
-menuKomponente.args = {}
+export const menuKomponente = Template.bind({});
+menuKomponente.args = {
+    isVertical: false,
+    buttonLabels: ['HTML', 'CSS', 'JavaScript']
+};
+
