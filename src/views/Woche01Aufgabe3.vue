c2a2b0edb2a93720f4999c676469d77954a66f15
d-fabian <fabian.dungs@gmx.de>
2022-03-12 10:10:56 +0100

    switch exercise 1.3 to use solutionWithOverlay component instead of exerciseSolution

--- src/views/Woche01Aufgabe3.vue
+++ src/views/Woche01Aufgabe3.vue
@@ -1,9 +1,11 @@
 <template>
-  <exercise-solution class="active">
+  <solution-with-overlay class="active">
     <template v-slot:exercise>
-      Mit welchem HTML- und CSS-Code kann man <a href="https://kaul.inf.h-brs.de/wem/assets/img/tribute.png" target="_blank">diesen</a> Wireframe möglichst exakt nachbilden?
-      Schreiben Sie Ihren CSS-Code direkt in die HTML-Datei. Für das Bild dürfen Sie die URL
-      <a href="https://kaul.inf.h-brs.de/wem/assets/img/tribute-img.png" target="_blank">/wem/assets/img/tribute-img.png</a> verwenden.
+      Mit welchem HTML- und CSS-Code kann man <a href="https://kaul.inf.h-brs.de/wem/assets/img/tribute.png"
+                                                 target="_blank">diesen</a> Wireframe möglichst exakt nachbilden?
+      Schreiben Sie Ihren CSS-Code direkt in die HTML-Datei. Für das Bild dürfen Sie die URL <a
+        href="https://kaul.inf.h-brs.de/wem/assets/img/tribute-img.png"
+        target="_blank">/wem/assets/img/tribute-img.png</a> verwenden.
     </template>
     <template v-slot:solution>
       <div id="solution">
@@ -14,29 +16,35 @@
         <img src="../assets/tribute-img.png" alt="tribute image"/>
       </div>
     </template>
-  </exercise-solution>
+  </solution-with-overlay>
 </template>
 
 <script>
-import ExerciseSolution from "@/components/ExerciseSolution";
+import SolutionWithOverlay from "@/components/SolutionWithOverlay";
 
 export default {
   name: "Woche01Aufgabe3",
   title: 'Web Engineering WS21/22 - Woche 1.3',
   components: {
-    ExerciseSolution
+    SolutionWithOverlay,
   },
 }
 </script>
 
 <style scoped>
-* {
-  margin: 0;
-}
 #solution {
+  display: flex;
+  flex-direction: column;
+  height: calc(100vh - 61px - 40px);
+  width: 100vw;
   background: lightgrey;
   text-align: center;
-  padding: 20px;
+  transition: 0.5s linear;
+}
+
+.active #solution {
+  height: calc(100vh - 40px);
+  width: calc(100vw - 378px);
 }
 
 h1 {
@@ -49,9 +57,11 @@ p {
 
 img {
   display: block;
+  margin: 20px;
   max-width: calc(100vw - 40px);
   transition: .5s linear;
 }
+
 .active img {
   max-width: calc(100vw - 418px);
 }
