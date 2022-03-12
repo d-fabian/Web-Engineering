a14b503df4888378b31fac84e3dce4b025947de0
d-fabian <fabian.dungs@gmx.de>
2022-03-12 10:39:15 +0100

    switch exercise 1.1 to use solutionWithOverlay component instead of exerciseSolution

--- src/views/Woche01Aufgabe1.vue
+++ src/views/Woche01Aufgabe1.vue
@@ -1,55 +1,70 @@
 <template>
-  <exercise-solution class="active">
+  <solution-with-overlay class="active">
     <template v-slot:exercise>
-      <ol>
-        <li class="exerciseBulletPoints">Mit welchen fachlichen Argumenten wurde das WWW-Proposal von TBL abgelehnt?
-        </li>
-        <li class="exerciseBulletPoints">Was sind die fachlichen Argumente, warum das WWW dennoch ein Erfolg wurde?
-        </li>
-        <li class="exerciseBulletPoints">Was wäre der Preis für die garantierte Verhinderung von “broken links“?</li>
-      </ol>
+      Siehe Aufgabenstellung direkt auf der Seite.
     </template>
     <template v-slot:solution>
-      <ol>
-        <li class="solutionTopBulletPoints"></li>
-        <ul class="solutionBulletPoints">
-          <li>TBL hat die Wagheit des Konzepts nicht verstanden<br>(er verwechselt es mit der Evolvierbarkeit)</li>
-          <li>Die Hypertext-Community hatte den Eindruck,<br>dass sie schon mehr erreicht hatten als TBL (insbesondere
-            wegen 'broken Links')
+      <div class="container">
+        <ol>
+          <li class="exerciseBulletPoints">Mit welchen fachlichen Argumenten wurde das WWW-Proposal von TBL abgelehnt?
           </li>
-        </ul>
-        <li class="solutionTopBulletPoints"></li>
-        <ul class="solutionBulletPoints">
-          <li>Die Unterteilung in die verschiedenen orthogonalen Technologien:</li>
           <ul class="solutionBulletPoints">
-            <li>HTTP</li>
-            <li>HTML</li>
-            <li>eindeutige Identifikation (URI)</li>
+            <li>TBL hat die Wagheit des Konzepts nicht verstanden<br>(er verwechselt es mit der Evolvierbarkeit)</li>
+            <li>Die Hypertext-Community hatte den Eindruck,<br>dass sie schon mehr erreicht hatten als TBL (insbesondere
+              wegen 'broken Links')
+            </li>
           </ul>
-        </ul>
-        <li class="solutionTopBulletPoints"></li>
-        <ul class="solutionBulletPoints lastPoint">
-          <li>zentrale Geschlossenheit</li>
-          <li>Kontrolle</li>
-        </ul>
-      </ol>
+          <li class="exerciseBulletPoints">Was sind die fachlichen Argumente, warum das WWW dennoch ein Erfolg wurde?
+          </li>
+          <ul class="solutionBulletPoints">
+            <li>Die Unterteilung in die verschiedenen orthogonalen Technologien:</li>
+            <ul class="solutionBulletPoints">
+              <li>HTTP</li>
+              <li>HTML</li>
+              <li>eindeutige Identifikation (URI)</li>
+            </ul>
+          </ul>
+          <li class="exerciseBulletPoints">Was wäre der Preis für die garantierte Verhinderung von “broken links“?</li>
+          <ul class="solutionBulletPoints lastPoint">
+            <li>zentrale Geschlossenheit</li>
+            <li>Kontrolle</li>
+          </ul>
+        </ol>
+      </div>
     </template>
-  </exercise-solution>
+  </solution-with-overlay>
 </template>
 
 <script>
-import ExerciseSolution from "@/components/ExerciseSolution";
+import SolutionWithOverlay from "@/components/SolutionWithOverlay";
 
 export default {
   name: "Woche01Aufgabe1",
   title: 'Web Engineering WS21/22 - Woche 1.1',
   components: {
-    ExerciseSolution
+    SolutionWithOverlay,
   },
 }
 </script>
 
 <style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 61px - 40px);
+  width: 100vw;
+  transition: 0.5s linear;
+  font-size: 20px;
+}
+
+.active .container {
+  height: calc(100vh - 40px);
+  width: calc(100vw - 378px);
+}
+
 ol {
   display: flex;
   flex-direction: column;
@@ -69,10 +84,6 @@ li::marker {
   margin: 20px 0;
 }
 
-.solutionTopBulletPoints {
-  margin-top: 20px;
-  font-weight: bold;
-}
 
 .solutionBulletPoints {
   margin-left: 30px;
