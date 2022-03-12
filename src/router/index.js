6f1a69bb82403524c7d9e33deafb061df79526cd
d-fabian <fabian.dungs@gmx.de>
2022-03-12 09:24:12 +0100

    remove unnecessary About component from vue examples

--- src/router/index.js
+++ src/router/index.js
@@ -12,15 +12,6 @@ const routes = [
         component: Home
     },
     {
-        path: '/about',
-        name: 'About',
-        meta: 'About',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/About')
-    },
-    {
         path: '/woche1aufgabe1',
         name: 'Woche 1 - Aufgabe 1',
         meta: 'Woche 1 - Aufgabe 1: Fachliche Argumentation über Erfolgsprinzipien des WWW',
